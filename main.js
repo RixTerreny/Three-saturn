66b060e413f9ae8e0a16c8eac694e1f60e23e733
RixTerreny <riccardotrave43@gmail.com>
2023-03-31 06:07:02 +0200

    flyController

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import * as THREE from 'three';
 
 import{OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
+import{FlyControls} from 'three/examples/jsm/controls/FlyControls'
+
 const scene = new THREE.Scene();
 
 const renderer = new THREE.WebGLRenderer();
@@ -11,14 +13,24 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 //camera
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
-camera.position.z = 10;
-camera.position.y = 5;
+const camera = new THREE.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 0.1, 20000 );
+camera.position.z = 3000;
+camera.position.y = 137;
 
 //scroll
 document.addEventListener( 'mousewheel', (event) => {
   camera.position.z +=event.deltaY/100;
 });
+
+// resize window
+window.addEventListener( 'resize', onWindowResize );
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
 // Background
 const spaceTexture = new THREE.TextureLoader().load('space.jpg');
@@ -33,10 +45,11 @@ lightP.position.set( -10, 400, 900 ); // position the point light
 scene.add( lightP );
 
 //controls
-const controls = new OrbitControls(camera, renderer.domElement);
+ //const controls = new OrbitControls(camera, renderer.domElement);
+const flyControls = new FlyControls(camera, renderer.domElement);
 
 //objects
-const geometry = new THREE.SphereGeometry( 15, 60, 60);
+const geometry = new THREE.SphereGeometry( 550, 60, 60);
 const material = new THREE.MeshLambertMaterial( { color: 0x000000,side: THREE.DoubleSide } );
 const sphere = new THREE.Mesh( geometry, material );
 scene.add( sphere );
@@ -51,30 +64,36 @@ const material3 = new THREE.MeshNormalMaterial( { color: 0xffff00,} );
 const torusKnot = new THREE.Mesh( geometry3, material3 );
 scene.add( torusKnot );
 
+var loader = new THREE.TextureLoader();
+const texture = loader.load('./fire.jpg');
+texture.magFilter = THREE.NearestFilter; 
+texture.wrapS = THREE.ClampToEdgeWrapping; 
+texture.wrapT = THREE.ClampToEdgeWrapping;
 
-const geometry1 = new THREE.RingGeometry( 17, 50, 102);
-const material1 = new THREE.MeshLambertMaterial( { color: 0xffffff,side: THREE.DoubleSide } );
+
+const geometry1 = new THREE.RingGeometry( 1500, 580, 102);
+const material1 = new THREE.MeshLambertMaterial({map: texture,side: THREE.DoubleSide});
 const cube = new THREE.Mesh( geometry1, material1 );
 cube.rotation.x = -Math.PI / 2;
 scene.add( cube );
 
-
+//core
 function createCore(){
-  const geometry3 = new THREE.BoxGeometry( 0.8, 0.8, 0.8);
+  const geometry3 = new THREE.BoxGeometry( 3, 5, 3);
   const material3 = new THREE.MeshPhysicalMaterial( { color: 0x00ff00,emissive:0,roughness:0,transparent:true,opacity:0.40 } );
   const sphere3 = new THREE.Mesh( geometry3, material3 );
   scene.add( sphere3 );
 
 const [x, y, z] = Array(3)
     .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(18));
+    .map(() => THREE.MathUtils.randFloatSpread(400));
 
     sphere3.rotation.z += 0.01;
   sphere3.position.set(x, y, z);
   scene.add(sphere3);
 }
 
-Array(200).fill().forEach(createCore);
+Array(3000).fill().forEach(createCore);
 
 
 
@@ -96,19 +115,23 @@ function animate() {
   torusKnot.rotation.y += 0.03;
   //cube.rotation.x += 0.002;
 
-  controls.update();
+  flyControls.movementSpeed = 5;
+  flyControls.dragToLook = true;
+  flyControls.update(0.4)
+//controls.update();
 
 	renderer.render( scene, camera );
 }
 
+//star
 function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const geometry = new THREE.SphereGeometry(1.25, 24, 24);
   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
   const star = new THREE.Mesh(geometry, material);
 
   const [x, y, z] = Array(3)
     .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(750));
+    .map(() => THREE.MathUtils.randFloatSpread(7500));
 
   star.position.set(x, y, z);
   scene.add(star);
@@ -116,19 +139,20 @@ function addStar() {
 
 
 function addStar2() {
-  const geometry = new THREE.SphereGeometry(1.55, 24, 24);
+  const geometry = new THREE.SphereGeometry(3.55, 24, 24);
   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
   const star = new THREE.Mesh(geometry, material);
 
   const [x, y, z] = Array(3)
     .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(800)+200);
+    .map(() => THREE.MathUtils.randFloatSpread(10000)+200);
 
   star.position.set(x, y, z);
   scene.add(star);
 }
 
 
-Array(400).fill().forEach(addStar);
-Array(70).fill().forEach(addStar2);
+
+Array(6000).fill().forEach(addStar);
+Array(670).fill().forEach(addStar2);
 animate();
